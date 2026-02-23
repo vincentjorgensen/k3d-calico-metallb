b0bad254cb13ed6bbb3f8cb865d74458a6ae76e8
Vincent Jorgensen <vincentjorgensen@users.noreply.github.com>
2026-02-22 19:25:52 -0800

    bump k3s versions

--- functions.sh
+++ functions.sh
@@ -84,10 +84,10 @@ export K3S_VER K3S_VER_132 K3S_VER_133 K3S_VER_134 K3S_VER_135
 CALICO_VER="3.30.6"                        # https://github.com/projectcalico/calico/tags # Don't forget to download new manifest and put in $K3D_DIR when upgrading
 #CALICO_VER="3.30.5"                        # https://github.com/projectcalico/calico/tags # Don't forget to download new manifest and put in $K3D_DIR when upgrading
                                            # https://raw.githubusercontent.com/projectcalico/calico/v3.30.5/manifests/calico.yaml
-K3S_VER_132="v1.32.11-k3s1"                # https://hub.docker.com/r/rancher/k3s/tags?name=v1.32
-K3S_VER_133="v1.33.7-k3s1"                 # https://hub.docker.com/r/rancher/k3s/tags?name=v1.33
-K3S_VER_134="v1.34.3-k3s1"                 # https://hub.docker.com/r/rancher/k3s/tags?name=v1.34
-K3S_VER_135="v1.35.0-k3s1"                 # https://hub.docker.com/r/rancher/k3s/tags?name=v1.35
+K3S_VER_132="v1.32.12-k3s1"                # https://hub.docker.com/r/rancher/k3s/tags?name=v1.32
+K3S_VER_133="v1.33.8-k3s1"                 # https://hub.docker.com/r/rancher/k3s/tags?name=v1.33
+K3S_VER_134="v1.34.4-k3s1"                 # https://hub.docker.com/r/rancher/k3s/tags?name=v1.34
+K3S_VER_135="v1.35.1-k3s1"                 # https://hub.docker.com/r/rancher/k3s/tags?name=v1.35
 MLB_VER="v0.15.3"                          # https://github.com/metallb/metallb/tags
                                            # https://raw.githubusercontent.com/metallb/metallb/v0.14.9/config/manifests/metallb-native.yaml 
                                            # metallb-native.address-pool.template.yaml
